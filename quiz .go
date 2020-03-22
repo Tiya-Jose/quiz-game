3134301f7798551e4a68c04a849060b04c461bf5
Tiya-Jose <mariyatiyajose@gmail.com>
2020-03-22 20:07:16 +0530

    added timer

--- quiz .go
+++ quiz .go
@@ -1,13 +1,20 @@
 package main
-import ("flag"
-"os"
-"log"
-"encoding/csv"
-"io"
-"fmt"
-"strconv"
 
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strconv"
+	"time"
 )
+
+var total = 10
+var right = 0
+var ch = make(chan []string, 10)
+
 func processCSV(rc io.Reader) (ch chan []string) {
 	ch = make(chan []string, 10)
 	go func() {
@@ -20,32 +27,69 @@ func processCSV(rc io.Reader) (ch chan []string) {
 					break
 				}
 				log.Fatal(err)
-				
+
 			}
 			ch <- rec
 		}
 	}()
-	return
+	return ch
 }
-
-func main(){
-	var total=0
-	var right=0
-	quizfile:=flag.String("quizFile","prob.csv","csv quiz filename ")
-	flag.Parse()
-	file, err := os.Open(*quizfile) // For read access.TODO: remove this opening
-     if err != nil {
-	log.Fatal(err)
-	}
-	var ans int
+func process(file *os.File) {
 	for rec := range processCSV(file) {
-		total++
-		fmt.Println(rec[0])
-		fmt.Scan(&ans)
-		anss:=strconv.Itoa(ans)
-		if anss==rec[1]{
-right++
+		ch <- rec
+	}
+}
+func checkTime(ticker *time.Ticker) {
+	select {
+	case <-ticker.C:
+		fmt.Println("Your timer expired!!!")
+		fmt.Printf("Score: %d/%d\n", right, total)
+		close(ch)
+		ticker.Stop()
+		os.Exit(1)
+		return
+	}
+}
+func startTimer(start string, timer *int) *time.Ticker {
+	if start == "y" {
+		duration := int(time.Second) * *timer
+		newDuration := time.Duration(duration)
+		return time.NewTicker(newDuration)
+	}
+	return time.NewTicker(0)
+}
+func main() {
+	var start string
+	quizfile := flag.String("quizFile", "problems.csv", "csv quiz filename ")
+	timer := flag.Int("timer", 5, "time to complete the quiz")
+
+	flag.Parse()
+	defer close(ch)
+
+	file, err := os.Open(*quizfile) // For read access.TODO: remove this opening
+	if err != nil {
+		log.Fatal(err)
+	}
+	intro := fmt.Sprintf("Press y to start your timer for %d seconds", *timer)
+	fmt.Println(intro)
+	fmt.Scan(&start)
+	ticker := startTimer(start, timer)
+	defer ticker.Stop()
+	go process(file)
+
+	for {
+		select {
+		case rec := <-ch:
+			go checkTime(ticker)
+			var ans int
+			fmt.Println(rec[0])
+			fmt.Scan(&ans)
+			anss := strconv.Itoa(ans)
+			if anss == rec[1] {
+				right++
+			}
+
 		}
-		}
-		fmt.Printf("Mark: %d/%d\n",right,total)
+	}
+
 }
